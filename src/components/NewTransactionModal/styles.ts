da615586b4f9ca7a05559686720614d1eea8bb8d
André Viapiana <andreviapiana@gmail.com>
2023-02-06 17:50:01 -0300

    🔨refactor: Botões Entrada e Saída do tipo Radio (Radix UI Radio Group)

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -66,7 +67,7 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${(props) => props.theme['gray-500']};
 `
 
-export const TransactionType = styled.div`
+export const TransactionType = styled(RadioGroup.Root)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   gap: 1.6rem;
@@ -77,9 +78,11 @@ interface TransactionTypeButtonProps {
   variant: 'income' | 'outcome'
 }
 
-export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+export const TransactionTypeButton = styled(
+  RadioGroup.Item,
+)<TransactionTypeButtonProps>`
   background: ${(props) => props.theme['gray-700']};
-  padding: 1.6rem;
+  padding: 1rem;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -88,11 +91,24 @@ export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
   cursor: pointer;
   border: 0;
   color: ${(props) => props.theme['gray-300']};
-
   svg {
     color: ${(props) =>
       props.variant === 'income'
         ? props.theme['green-300']
         : props.theme['red-300']};
+  }
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${(props) => props.theme['gray-600']};
+  }
+  &[data-state='checked'] {
+    color: ${(props) => props.theme.white};
+    background: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-500']
+        : props.theme['red-500']};
+    svg {
+      color: ${(props) => props.theme.white};
+    }
   }
 `
