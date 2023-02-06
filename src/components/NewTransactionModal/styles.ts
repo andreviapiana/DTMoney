d8b79fc212a4fddd8db1dd47caabb23f60523616
André Viapiana <andreviapiana@gmail.com>
2023-02-06 17:30:52 -0300

    ✨feat: Botões de Entrada e Saída do Modal

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -8,31 +8,35 @@ export const Overlay = styled(Dialog.Overlay)`
   inset: 0;
   background: rgba(0, 0, 0, 0.75);
 `
-
 export const Content = styled(Dialog.Content)`
   min-width: 51.2rem;
   border-radius: 6px;
   padding: 4rem 4.8rem;
   background: ${(props) => props.theme['gray-800']};
+
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+
   form {
     margin-top: 3.2rem;
     display: flex;
     flex-direction: column;
     gap: 1.6rem;
+
     input {
       border-radius: 6px;
       border: 0;
       background: ${(props) => props.theme['gray-900']};
       color: ${(props) => props.theme['gray-300']};
       padding: 1.6rem;
+
       &::placeholder {
         color: ${(props) => props.theme['gray-500']};
       }
     }
+
     button[type='submit'] {
       height: 50px;
       border: 0;
@@ -43,6 +47,7 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 2rem;
       cursor: pointer;
+
       &:hover {
         background: ${(props) => props.theme['green-700']};
         transition: background-color 0.2s;
@@ -50,7 +55,6 @@ export const Content = styled(Dialog.Content)`
     }
   }
 `
-
 export const CloseButton = styled(Dialog.Close)`
   position: absolute;
   background: transparent;
@@ -60,4 +64,35 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${(props) => props.theme['gray-500']};
+`
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1.6rem;
+  margin-top: 0.8rem;
+`
+
+interface TransactionTypeButtonProps {
+  variant: 'income' | 'outcome'
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme['gray-700']};
+  padding: 1.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.8rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme['gray-300']};
+
+  svg {
+    color: ${(props) =>
+      props.variant === 'income'
+        ? props.theme['green-300']
+        : props.theme['red-300']};
+  }
 `
