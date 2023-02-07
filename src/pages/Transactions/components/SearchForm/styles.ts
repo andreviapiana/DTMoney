ffa04ae260a68871159e8fa39fc5476adb088874
André Viapiana <andreviapiana@gmail.com>
2023-02-07 16:39:48 -0300

    ✨feat: Inclusão do React Hook Form e Zod no SearchForm

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -2,30 +2,41 @@ import styled from 'styled-components'
 
 export const SearchFormContainer = styled.form`
   display: flex;
-  gap: 1rem;
+  gap: 1.6rem;
+
   input {
     flex: 1;
     border-radius: 6px;
     border: 0;
     background: ${(props) => props.theme['gray-900']};
     color: ${(props) => props.theme['gray-300']};
-    padding: 1rem;
+    padding: 1.6rem;
+
     &::placeholder {
       color: ${(props) => props.theme['gray-500']};
     }
   }
+
   button {
     display: flex;
     align-items: center;
-    gap: 0.75rem;
+    gap: 1.2rem;
+
     border: 0;
-    padding: 1rem;
+    padding: 1.6rem;
     background: transparent;
     border: 1px solid ${(props) => props.theme['green-300']};
     color: ${(props) => props.theme['green-300']};
     font-weight: bold;
     border-radius: 6px;
-    &:hover {
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme['green-500']};
       border-color: ${(props) => props.theme['green-500']};
       color: ${(props) => props.theme.white};
